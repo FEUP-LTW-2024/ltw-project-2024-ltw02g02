de1ffd928ade66a9e2948782d64e8a68505d342f
Yussee-1 <up202207784@up.pt>
2024-04-16 23:06:15 +0100

    Added menu button animations

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
-document.getElementById('menulist').addEventListener('click',function() {
-    document.getElementById('menu').classList.toggle('open')
+const list = document.getElementById('menulist');
+const cross = document.getElementById('menuclosed');
+const menu = document.getElementById('menu');
+list.addEventListener('click',function() {
+    this.classList.toggle('dissapear');
+    cross.classList.toggle('dissapear');
+    menu.classList.toggle('open');
+})
+cross.addEventListener('click',function() {
+    this.classList.toggle('dissapear');
+    list.classList.toggle('dissapear');
+    menu.classList.toggle('open');
 })
