8201eb2a5e268db8182932041fdbdf25b18a321a
root <root@PC-Guilherme>
2024-04-16 08:46:57 +0100

    Start login & register

--- login.php
+++ login.php
@@ -1,8 +1,28 @@
 <!DOCTYPE html>
 <head>
     <Title>Log In</Title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <h1>Log In Page</h1>
+    <main>
+        <div>
+            <i><a href="signup.php">Não tenho uma conta</a></i>
+        </div>
+        <div class="email">
+            <i>Email</i>
+            <form class="searchbar">
+                <input type="text" name="q" placeholder="">
+            </form>
+        </div>
+        <div class="password">
+            <i>Password</i>
+            <form class="searchbar">
+                <input type="text" name="q">
+            </form>
+        </div>
+        <div>
+            <button type="submit">Entrar</button>
+        </div>
+    </main>
     <h3><a href="index.php">Back</a></h3>
 </body>
