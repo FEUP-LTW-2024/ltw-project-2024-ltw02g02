103b35cbadb921f8bc2690ecfa77705d4f1fefaf
Guilherme Rego <up202207041@fe.up.pt>
2024-04-21 16:10:04 +0100

    Functional login & logout system

--- index.php
+++ index.php
@@ -1,57 +1,112 @@
 <?php
-    $db = new PDO("sqlite:database.db");
-    $stmt = $db->prepare("SELECT * FROM Items ORDER BY RANDOM() LIMIT 1");
-    $stmt->execute();
-    $item = $stmt->fetch();
-    $stmt = $db->prepare("SELECT * FROM Users WHERE :seller_id = user_id");
-    $stmt->bindParam(':seller_id',$item['seller_id']);
-    $stmt->execute();
-    $user = $stmt->fetch();
+    session_start();
 ?>
 
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8">
-    <title>hand2hand</title>
+    <title>Hand2Hand</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar">
-        <div class="navbar-left">
-            <img src="images/menu.png" href="bota.html">
-            <i><a href="index.php">hand2hand</a></i>
-        </div>
-        <form class="searchbar" action="/search" method="get">
-            <input type="submit" value="">
-            <input type="text" name="q" placeholder="Search...">
-        </form>          
-        <div class="navbar-right">
-            <i><a href="login.php">Log In</a></i>
-            <i><a href="signup.php">Sign Up</a></i>
+        <div class="navbar-inner">
+            <div class="navbar-left">
+                <div class="menuicons">
+                    <img id="menulist" src="icons/list.svg">
+                    <img id="menuclosed" class="dissapear" src="icons/x.svg">
+                 </div>
+                <li><a class="logo" href="index.php">Hand2Hand</a></li> 
+                <form class="searchbar" action="/search" method="get">
+                    <input type="text" name="q" placeholder="Search...">
+                    <i class="bi bi-search"></i>
+                </form>
+            </div>    
+            <div class="navbar-right">
+                <?php if (isset($_SESSION['username'])): ?>
+                    <li><a href="profile.php"><img id="pfp" src="<?php echo htmlspecialchars($_SESSION['pfp_url']); ?>"> Profile</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php else: ?>
+                    <li><a href="login.html">Log In</a></li>
+                    <li><a href="signup.html">Sign Up</a></li>
+                <?php endif; ?>
+            </div>
         </div>
     </nav>
+    <div id="menu" class="menu">Guilherme é Gay</div>
+    <script src="script/script.js"></script>
     <main>
-        <h1> FEATURED ITEM </h1>
+        <h1> FEATURED ITEMS </h1>
         <div class="featured">
-            <?php
-                echo "<div class='imgbox'>";
-                echo "<img src=" . $item['image_url'] . ">";
-                echo "</div>";
-                echo "<div class='textbox'>";
-                echo "<h1 class='title'>" . $item['title'] . "</h1>";
-                echo "<p class='published'> Published " . date('d-m-Y H:i:s',strtotime($item['publish_date'])) . "</p>";
-                echo "<p class='description'>" . $item['description'] . "</p>";
-                echo "<p class='location'>" . $item['location'] . "</p>";
-            ?>
-            <div class="usercontainer">
-                <?php
-                    echo "<img class='pfp' src=" . $user['pfp_url'] . ">";
-                    echo "<p class='username'>" . $user['username'] . "</p>";
-                    echo "</div>";
-                ?>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+            <article>
+                <div class="imgbox">
+                    <img src="images/ps2.webp">
+                </div>
+                <a class="title" href="item.html">Title Text Example</a>
+                <p class ="small-text">Praia da Vitória</p>
+                <p class ="small-text">Published 01-01-2000 00:00</p>
+            </article>
+                
             </div>
         </div>
     </main>
